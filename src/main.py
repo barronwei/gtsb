c65c9357cb9f2c4c71b71859941eecdd14b88f20
barronwei <barronwei@outlook.com>
2019-12-03 11:16:56 -0500

    Minor Commit

--- src/main.py
+++ src/main.py
@@ -8,18 +8,31 @@ drop_cols = [1, 2, 4, 5, 7, 10, 11, 14, 15]
 
 data.drop(data.columns[drop_cols], axis=1, inplace=True)
 
-is_fulfilled = data[data.columns[0]] != "N/A"
-complete = data[is_fulfilled]
-cols = complete.columns
 
-len(complete)
-complete.head()
+class Analysis(object):
+    pass
 
-sent_text = [tb(s).sentiment.polarity for s in complete[complete_cols[1]]]
-sent_head = [tb(s).sentiment.polarity for s in complete[complete_cols[2]]]
-comp = complete.assign(sentiment_text=sent_text, sentiment_head=sent_head)
 
-comp_cols = comp.columns
-stats = [comp[c].describe() for c in comp_cols]
+def stats(data):
+    res = Analysis()
 
-comp.corr()
+    sent_text = [tb(s).sentiment.polarity for s in data[data.columns[1]]]
+    sent_head = [tb(s).sentiment.polarity for s in data[data.columns[2]]]
+
+    data = data.assign(sentiment_text=sent_text, sentiment_head=sent_head)
+    desc = [data[c].describe() for c in data.columns]
+
+    res.corr = data.corr()
+    res.desc = desc
+
+    return res
+
+
+is_settled = data[data.columns[0]] != "N/A"
+settled = data[is_fulfilled]
+
+is_lacking = data[data.columns[0]] == "N/A"
+lacking = data[is_lacking]
+
+stats_settled = stats(settled)
+stats_lacking = stats(lacking)
